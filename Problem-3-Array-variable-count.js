a9b2fa0e919863c3a38d9446e646ba2a48f27531
alkausarrabbi <alkausarrabbi24@gmail.com>
2021-08-13 13:02:31 +0600

    problem-3

---
+++ Problem-3-Array-variable-count.js
@@ -0,0 +1,21 @@
+// simple way
+var ages=[21,22,34,45,37,79,51,46];
+console.log(ages.length);
+
+
+// another way
+var names=["samia","rahat","rony","yousuf","arafat"];
+var arraycount=names.length;
+console.log(arraycount);
+
+
+
+// using function
+function elements(array){
+    var elementsOfArray=array.length;
+    return elementsOfArray;
+}
+var array=[11,22,33,44,55,66,77,88,99];
+var count=elements(array);
+console.log(count);
+
