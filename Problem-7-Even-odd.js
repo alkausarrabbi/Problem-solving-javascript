a6fd00f477db166b8985948a4e90d3320b171def
alkausarrabbi <alkausarrabbi24@gmail.com>
2021-08-14 12:09:20 +0600

    Problem-7 added

---
+++ Problem-7-Even-odd.js
@@ -0,0 +1,27 @@
+// for লুপ দিয়ে odd number এবং even number প্রিন্ট করার একটি প্রোগ্রাম লেখো। 
+// Using while loop
+// odd numbers
+var i=1;
+while (i<=10){
+    console.log("Odd NUmbers :",i);
+    i+=2;
+}
+
+//  even numbers
+var i=0;
+while (i<=10) {
+ console.log("Even numbers :",i);
+    i+=2;
+}
+
+
+// using for loop
+// even numbers
+for(var i=0;i<=10;i+=2){
+        console.log("even num",i);
+    }
+
+    // odd numbers
+    for (var i=1;i<=10;i+=2){
+        console.log("odd num",i);
+    }
