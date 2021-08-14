54f5b564494a3376c684b23ea12ce2bf780b7d62
alkausarrabbi <alkausarrabbi24@gmail.com>
2021-08-14 10:59:54 +0600

    problem-6 added

---
+++ Problem-6-Array-add-remove.js
@@ -0,0 +1,18 @@
+// একটি এরের একেবারে শেষ থেকে ভ্যালু রিমুভ করার একটি প্রোগ্রাম লেখো । 
+// array last digit remove
+var numbers=[11,79,121,99,22,212,34,680,87];
+numbers.pop();
+console.log(numbers);
+
+// last digit add
+numbers.push(21);
+console.log(numbers);
+
+// arrya first digit remove
+var names =["tony","mark","thor","natasha","halk","steve"];
+names.shift();
+console.log(names);
+
+// first digit add
+names.unshift("batman");
+console.log(names);
