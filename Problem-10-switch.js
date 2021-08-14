1e4173c9eab431acc695ec5e532d16b6b652e959
alkausarrabbi <alkausarrabbi24@gmail.com>
2021-08-14 14:00:38 +0600

    Problem-10 added

---
+++ Problem-10-switch.js
@@ -0,0 +1,13 @@
+var color="black";
+switch(color){
+    case "blue" :
+        console.log("Color is Blue");
+        break;
+        case "white" :
+            console.log("Color is White");
+            break;
+            case "black" :
+                console.log("Color is Black");
+                break;
+        
+        }
---
+++ Problem-9-Object.js
@@ -0,0 +1,13 @@
+
+var computer={
+    name : "mac book pro 2016",
+    processor : "intel i5",
+    ram : "8gb",
+    ssd : "256gb",
+    display: "13-inch",
+    price :120000,
+}
+console.log("MacBook price is :",computer.price);
+computer.price=110000;
+var newPrice=computer.price;
+console.log("MacBook new Price is :",newPrice);
